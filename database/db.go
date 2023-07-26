7de2851035d60d9ddb7cf94bdd2396a6b8af4cb1
Anthony Sánchez <asanchez047@puce.edu.ec>
2023-07-26 09:38:25 -0500

    Creacion de funcion que permite abrin coneccion con el driver junto a su credenciales

--- database/db.go
+++ database/db.go
@@ -1,1 +1,24 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const dataSourceName = "go_mysql_user:go_mysql_password@tcp(localhost:3306)/go_mysql_db"
+const driverName = "mysql"
+
+var db *sql.DB
+
+func Connect() {
+
+	conect, err := sql.Open(driverName, dataSourceName)
+
+	if err != nil {
+		panic(err)
+	}
+	db = conect
+	fmt.Println("Database Open  Success")
+}
