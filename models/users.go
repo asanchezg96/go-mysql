dce29a2a1a4da30a59ca66a7943457b985a2ef22
Anthony Sánchez <asanchez047@puce.edu.ec>
2023-07-26 10:24:48 -0500

    Creacion de Schema users en una variable

---
+++ models/users.go
@@ -0,0 +1,11 @@
+package models
+
+const Schema = `
+	CREATE TABLE IF NOT EXISTS users (
+		id INT (6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(50) NOT NULL, 
+		email VARCHAR(50) NOT NULL,
+		password VARCHAR(50) NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);
+`
