55c1778d53ced4a1b7f8ac853f579bf6a45e29ab
Anthony Sánchez <asanchez047@puce.edu.ec>
2023-07-26 10:24:12 -0500

    Uso de funcion enviado por parametro Schema del archivo users del modelo

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"go-mysql-crud/database"
+	"go-mysql-crud/models"
 )
 
 func main() {
 	fmt.Println("GO-MYSQL-CRUD")
 	database.Connect()
 	database.Ping()
+
+	database.CreateTable(models.Schema)
 	database.Close()
 }
