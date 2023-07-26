0da61f0fe3d531c85920ae96e52c8ca73dd91a30
Anthony Sánchez <asanchez047@puce.edu.ec>
2023-07-26 16:53:06 -0500

    Creacion de metodo receptor para ingresar un objeto en la tabla users directamente a la base de datos

--- models/users.go
+++ models/users.go
@@ -1,5 +1,7 @@
 package models
 
+import "go-mysql-crud/database"
+
 const Schema = `
 	CREATE TABLE IF NOT EXISTS users (
 		id INT (6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -29,4 +31,20 @@ func NewUser(name, password, email string) *User {
 		Email:    email,
 	}
 	return user
+}
+
+// Create function to create user in tabble users
+func CreateUser(name, password, email string) *User {
+	user := NewUser(name, password, email)
+	user.insert()
+	return user
+}
+
+// Create method to insert data in table users
+func (user *User) insert() {
+	sql := "INSERT INTO users SET name= ?, password=?, email=?"
+	_, err := database.Exec(sql, user.Name, user.Password, user.Email)
+	if err != nil {
+		panic(err)
+	}
 }
