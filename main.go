abc1a1614266b483efae74a4173c7c25939ec92b
Anthony Sánchez <asanchez047@puce.edu.ec>
2023-07-26 16:27:04 -0500

    Creacion de objeto user con constructor

--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"fmt"
 	"go-mysql-crud/database"
+	"go-mysql-crud/models"
 )
 
 func main() {
 	fmt.Println("GO-MYSQL-CRUD")
 	database.Connect()
 	database.Ping()
-
 	//database.CreateTable(models.Schema)
-
 	//Verify if table exists
-	database.ExistsTable("users")
+	//database.ExistsTable("users")
+
+	//create new user
+	user := models.NewUser("Jhon", "123456", "jhon@gmai.com")
+	fmt.Println(user)
 	database.Close()
 }
