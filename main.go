c24c0fcab3bf5641a96e4037b7e958ed094a21ff
Anthony Sánchez <asanchez047@puce.edu.ec>
2023-07-26 22:01:01 -0500

    Creacion tabla, usuarios, editar, elminiar

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Println("GO-MYSQL-CRUD")
 	database.Connect()
 	database.Ping()
-	//database.CreateTable(models.Schema)
+	database.CreateTable(models.Schema)
 	//Verify if table exists
 	//database.ExistsTable("users")
 
