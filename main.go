79c52d691b11ba7acc9c005b84268f1c217f94ca
Anthony Sánchez <asanchez047@puce.edu.ec>
2023-07-26 19:09:00 -0500

    Uso de funcion truncate

--- main.go
+++ main.go
@@ -14,11 +14,15 @@ func main() {
 	//Verify if table exists
 	//database.ExistsTable("users")
 
+	//Truncate table
+	database.TruncateTable("users")
+
 	//create new user
 	//user := models.NewUser("Jhon", "123456", "jhon@gmai.com")
 	user := models.CreateUser("Tony", "pass", "tony@gmail.com")
 	fmt.Println(user)
 	user2 := models.CreateUser("Jhon", "pass", "jhon@gmail.com")
 	fmt.Println(user2)
+	fmt.Println(models.ListUsers())
 	database.Close()
 }
